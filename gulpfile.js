aa2b3c017f70913311da8ac6e389b53590ec4b67
Iryna Bryzhan <iryna97bryzhan@gmail.com>
2024-06-26 17:25:52 +0300

    +gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,40 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const watch = require('gulp-watch');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const watch = require("gulp-watch");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const htmlmin = require("gulp-htmlmin");
+const rename = require("gulp-rename");
 
-// Задача для компиляции SCSS в CSS
-gulp.task('sass', function() {
-    return gulp.src('scss/**/*.scss') // Источник SCSS файлов
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('style')); // Папка назначения для CSS файлов
+gulp.task("sass", function () {
+  return gulp
+    .src("scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist/style"));
 });
 
-// Задача для наблюдения за изменениями в SCSS файлах
-gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+gulp.task("minify-js", function () {
+  return gulp
+    .src("js/**/*.js")
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist/js"));
 });
 
-// Задача по умолчанию, запускающая компиляцию и наблюдение
-gulp.task('default', gulp.series('sass', 'watch'));
+gulp.task("minify-html", function () {
+  return gulp
+    .src("*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch("scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("js/**/*.js", gulp.series("minify-js"));
+  gulp.watch("*.html", gulp.series("minify-html"));
+});
+
+gulp.task("default", gulp.series("sass", "minify-js", "minify-html", "watch"));
--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const mobileMenu = document.querySelector('.mobile-menu');
-
-    burgerMenu.addEventListener('click', function () {
-        burgerMenu.classList.toggle('active');
-        mobileMenu.classList.toggle('active');
-    });
-});
